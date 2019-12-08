31e15a863384e115d9bec5366709825d084cd7d2
FC <teleporterone+gl@gmail.com>
2019-12-08 18:48:22 +0000

    Removed old comment

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -14,13 +14,6 @@ import {filter, map, startWith, switchMap} from 'rxjs/operators';
 })
 export class AppComponent {
   myControl = new FormControl();
-  /*
-  options: string[][] = [['One', '1'], ['Two', '2'], ['Three', '3']
-    ,['Four', '4'], ['Five', '5'], ['Six', '6']
-    ,['Seven', '7'], ['Eight', '8'], ['Nine', '9']
-    ,['Ten', '10'], ['Eleven', '11'], ['Twelve', '12']
-  ];
-  */
   options: Observable<string[][]> = of([['One', '1'], ['Two', '2'], ['Three', '3']
     ,['Four', '4'], ['Five', '5'], ['Six', '6']
     ,['Seven', '7'], ['Eight', '8'], ['Nine', '9']
